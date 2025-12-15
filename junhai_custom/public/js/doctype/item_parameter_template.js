07dd92d140a17096fd0a4f0e02293c47703c3f11
dev <dev@bit.js.cn>
2025-12-15 11:09:04 +0800

    feat(item-parameters): improve UI layout and add search indexing
    - Add search index to parameter name fields for better query performance
    - Configure column widths in template definition list view for improved readability
    - Enable item_group field in list view for Item Parameter Template
    - Add Chinese translations for "Join To Hash", "Readonly Value", and "mm"
    
    These changes enhance the user experience when managing item parameters and templates by improving list view layout and search capabilities.

--- junhai_custom/public/js/doctype/item_parameter_template.js
+++ junhai_custom/public/js/doctype/item_parameter_template.js
@@ -1,4 +1,31 @@
-// 文件名/脚本名: Item Parameter Template - Default Value Sync
+frappe.ui.form.on('Item Parameter Template', {
+    refresh: function (frm) {
+        if (!frm.doc.__islocal) {
+
+            frm.add_custom_button(__('创建物料申请单'), function () {
+
+                const template_name = frm.doc.name;
+
+                // 🌟 关键修正：使用 frappe.model.with_doctype 和 frappe.model.get_new_doc
+                frappe.model.with_doctype('New Item Request', function () {
+
+                    // 1. 在内存中创建一个新的 Doc 对象
+                    var new_request_doc = frappe.model.get_new_doc('New Item Request');
+
+                    // 2. 预设字段值
+                    // 预设字段名必须与 New Item Request DocType 上的字段名一致
+                    new_request_doc.template = template_name;
+                    // 命名系列字段会在这里被初始化 (即使 DocName 仍是 'New Item Request-00001')
+
+                    // 3. 导航到新的 Doc 对象
+                    // 我们使用 new_request_doc.name，它此时是一个临时客户端名称
+                    frappe.set_route('Form', 'New Item Request', new_request_doc.name);
+                });
+
+            }, __('操作'));
+        }
+    }
+});
 
 frappe.ui.form.on('Item Parameter Template Definition', { // 监听子表事件
     // // 监听所有动态输入字段的变动
